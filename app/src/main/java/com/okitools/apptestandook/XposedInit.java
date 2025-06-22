de4d8df08b3bcf3091442e5474333fbf51490470
MistoWest <mister.wes529@gmail.com>
2025-06-22 01:34:07 -0300

    :D

---
+++ app/src/main/java/com/okitools/apptestandook/XposedInit.java
@@ -0,0 +1,11 @@
+package com.okitools.apptestandook;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+public class XposedInit implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+        XposedBridge.log("Olá, mundo, Misto West");
+    }
+}
